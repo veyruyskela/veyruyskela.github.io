6aac828123f3ecbf8b9615ec5fe09f36b38b16d5
Aleksandr Yuryev <veyruyskela@gmail.com>
2019-11-26 13:50:02 +0400

    added upper sections

---
+++ src/js/script.js
@@ -0,0 +1,12 @@
+let hamburger = document.querySelector(".header__hamburger");
+let menu = document.querySelector(".header__menu");
+let flag = 0;
+hamburger.addEventListener("click", function () {
+    if ( flag === 0 ) {
+        menu.style.display = "block";
+        flag = 1;
+    } else if ( flag === 1) {
+        menu.style.display = "none";
+        flag = 0;
+    }
+});
